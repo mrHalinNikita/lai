460b5184d155891b4f51e9e254caf71f313b977f
Nikita Halin <halinn484@gmail.com>
2025-11-18 10:31:35 +0300

    refactor: code refactoring was performed

--- static/src/js/lai_calculator.js
+++ static/src/js/lai_calculator.js
@@ -1,22 +1,8 @@
-odoo.define('lai_estimator.lai_calculator', function (require) {
-    'use strict';
+/** @odoo-module alias=lai_estimator.lai_calculator **/
+$(document).ready(function () {
 
-    var publicWidget = require('web.public.widget');
-
-    publicWidget.registry.LAICalculatorWidget = publicWidget.Widget.extend({
-        selector: '.lai-upload-form',
-
-        start: function () {
-            this.$el.on('submit', this._onFormSubmit.bind(this));
-        },
-
-        _onFormSubmit: function (e) {
-            const $form = $(e.currentTarget);
-            const $submitBtn = $form.find('button[type="submit"]');
-
-            $submitBtn.html('<i class="fa fa-spinner fa-spin me-2"></i>Calculating...').prop('disabled', true);
-        }
+    $('.lai-upload-form').on('submit', function () {
+        const $submitBtn = $(this).find('button[type="submit"]');
+        $submitBtn.html('<i class="fa fa-spinner fa-spin me-2"></i>Calculating...').prop('disabled', true);
     });
-
-    return publicWidget.registry.LAICalculatorWidget;
 });
