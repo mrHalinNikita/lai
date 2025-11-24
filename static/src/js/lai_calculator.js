3a29537a2e2e882461f9f38f473a62bb642886b5
Nikita Halin <halinn484@gmail.com>
2025-11-24 21:40:00 +0300

    feat: display of the calibration block at the user's request

--- static/src/js/lai_calculator.js
+++ static/src/js/lai_calculator.js
@@ -1,5 +1,23 @@
 /** @odoo-module */
+
 $(document).on('submit', '.lai-upload-form', function () {
     const $btn = $(this).find('.lai-submit-btn');
     $btn.html('<span>Calculating…</span>').prop('disabled', true);
+});
+
+$(document).on('change', '#use_custom', function () {
+    const $fields = $('#custom-calibration-fields');
+    if (this.checked) {
+        $fields.slideDown(200);
+    } else {
+        $fields.slideUp(200);
+    }
+});
+
+$(document).ready(function () {
+    const $checkbox = $('#use_custom');
+    const $fields = $('#custom-calibration-fields');
+    if ($checkbox.length && $checkbox.is(':checked')) {
+        $fields.show();
+    }
 });
